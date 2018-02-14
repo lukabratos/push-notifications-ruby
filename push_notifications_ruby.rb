fdb631bd763d2192f5fd10bcda0973e4ecd161fe
Luka Bratos <luka@pusher.com>
2018-02-14 16:58:04 +0000

    Add testing secret key

--- push_notifications_ruby.rb
+++ push_notifications_ruby.rb
@@ -49,7 +49,7 @@ module Pusher
 end
 
 instance_id = '97c56dfe-58f5-408b-ab3a-158e51a860f2'
-secret_key = ''
+secret_key = '6033364526F02EDEF472461879CE485'
 
 pusher = Pusher::PushNotifications.new(instance_id, secret_key)
 publish_body = {
