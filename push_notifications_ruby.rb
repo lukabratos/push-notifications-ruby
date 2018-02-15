56fdc155886786c7e0b1835279400b6a3c7945e3
Luka Bratos <luka@pusher.com>
2018-02-15 15:32:49 +0000

    Remove example code

--- push_notifications_ruby.rb
+++ push_notifications_ruby.rb
@@ -161,18 +161,3 @@ module Pusher
     end
   end
 end
-
-instance_id = '97c56dfe-58f5-408b-ab3a-158e51a860f2'
-secret_key = '6033364526F02EDEF472461879CE485'
-
-pusher = Pusher::PushNotifications.new(instance_id, secret_key)
-
-publish_body = {
-  'apns': {
-    'aps': {
-      'alert': 'Hello!'
-    }
-  }
-}
-
-pusher.publish(%w[luka test], publish_body)
